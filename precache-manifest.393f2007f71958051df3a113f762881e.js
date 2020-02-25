4d8eaa74b4c467a30bb3ed30f03b2a3ca6267ed7
Alexanderqwer <llkkfi27456@gmail.com>
2020-02-25 19:19:22 +0200

    Updates

---
+++ precache-manifest.393f2007f71958051df3a113f762881e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b34442f7fcb28e03479c397915d90dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6d286c860a755a3d1ef",
+    "url": "/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "94cd8d0ee188d8c005c5",
+    "url": "/static/css/main.8270daf0.chunk.css"
+  },
+  {
+    "revision": "b6d286c860a755a3d1ef",
+    "url": "/static/js/2.63110c8f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.63110c8f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94cd8d0ee188d8c005c5",
+    "url": "/static/js/main.f81ce2a2.chunk.js"
+  },
+  {
+    "revision": "c6029566f5ce59d2d0e8",
+    "url": "/static/js/runtime-main.9c8e7735.js"
+  }
+]);
